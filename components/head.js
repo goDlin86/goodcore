6ff5bfa810a08b26852ebc258d93e8524bcf12d5
goDlin <godlin086@gmail.com>
2020-02-06 21:50:04 +0300

    main.css link

--- components/head.js
+++ components/head.js
@@ -28,7 +28,7 @@ const Head = props => (
     <meta property="og:image" content={props.ogImage || defaultOGImage} />
     <meta property="og:image:width" content="1200" />
     <meta property="og:image:height" content="630" />
-    <link rel="stylesheet" href="main.css" />
+    <link rel="stylesheet" href="/main.css" />
   </NextHead>
 );
 
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import Link from 'next/link';
+//import Link from 'next/link';
 import Head from '../components/head';
 //import Nav from '../components/nav';
 
