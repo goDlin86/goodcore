4ed144abffff833b10586c692356bf8f0c1cd080
goDlin <godlin086@gmail.com>
2020-02-10 18:52:20 +0300

    require

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
+require('dotenv').config()
 module.exports = {
-    env: {
-      /**
-      |--------------------------------------------------
-      | Set the client secret for your database here.
-      |
-      | Learn more about managing roles and creating private keys:
-      | https://docs.fauna.com/fauna/current/security/
-      |--------------------------------------------------
-      */
-      faunaDbSecret: process.env.DBSECRET,
-      faunaDbGraphQlEndpoint: 'https://graphql.fauna.com/graphql',
-    },
-  }
+  env: {
+    /**
+    |--------------------------------------------------
+    | Set the client secret for your database here.
+    |
+    | Learn more about managing roles and creating private keys:
+    | https://docs.fauna.com/fauna/current/security/
+    |--------------------------------------------------
+    */
+    faunaDbSecret: process.env.DBSECRET,
+    faunaDbGraphQlEndpoint: 'https://graphql.fauna.com/graphql',
+  },
+}
