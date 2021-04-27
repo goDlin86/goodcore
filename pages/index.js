c3f4769714e71c0d90ed809f705556f3934d0133
goDlin <godlin086@gmail.com>
2021-04-27 19:58:22 +0300

    on screen hook

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import { useSWRInfinite } from 'swr'
+import { useRef, useEffect } from 'react'
+
 import Head from '../components/head'
 import Album from '../components/album'
-import InfiniteScroll from 'react-infinite-scroll-component'
+import useOnScreen from '../hooks/useOnScreen'
 
 import ReactGA from 'react-ga'
 
@@ -18,15 +20,19 @@ function getDataByDate(data) {
   return { dates }
 }
 
-const Home = () => {
+const Home = ({ initialData }) => {
+  const ref = useRef()
+  const isVisible = useOnScreen(ref)
+
   ReactGA.pageview('/')
 
-  const { data, error, size, setSize } = useSWRInfinite((pageIndex, previousPageData) => {
+  const { data, error, size, setSize, isValidating } = useSWRInfinite((pageIndex, previousPageData) => {
       if (previousPageData && !previousPageData.after.length) return null
       if (pageIndex === 0) return `/api/get`
       return `/api/get?cursor=${previousPageData.after}`
     },
-    fetcher
+    fetcher,
+    { revalidateOnFocus: false, initialData }
   )
 
   const dataByDate = getDataByDate(data ? [].concat(...data) : [])
@@ -34,45 +40,50 @@ const Home = () => {
   const isLoadingInitialData = !data && !error
   const isLoadingMore =
     isLoadingInitialData ||
-    (size > 0 && data && typeof data[size - 1] === "undefined")
+    (size > 0 && data && typeof data[size - 1] === 'undefined')
   const isEmpty = data?.[0]?.length === 0
-  const isReachingEnd =
-    isEmpty || (data && data[data.length - 1]?.length < 8)
+  const isReachingEnd = data && data[data.length - 1]?.albums?.length < 8
+  const isRefreshing = isValidating && data && data.length === size
+
+  useEffect(() => {
+    if (isVisible && !isReachingEnd && !isRefreshing) {
+      setSize(size + 1)
+    }
+  }, [isVisible])//, isRefreshing])
 
   return (
-    <div>
-      <Head title="goodcore Releases" description="metalcore, deathcore, post-hardcore releases, link for download" />
+    <>
+      <Head title='goodcore Releases' description='metalcore, deathcore, post-hardcore releases, link for download' />
       
       <header>
         goo<span className="yellow">d</span>core
       </header>
       <section>
         <h1>Releases</h1>
-        <InfiniteScroll
-          dataLength={dataByDate.dates.reduce((count, d) => count + d.albums.length, 0)}
-          next={() => setSize(size + 1)}
-          hasMore={!isLoadingMore || !isReachingEnd}
-          style={{'overflow': 'unset'}}
-          scrollThreshold={0.95}
-          loader={<p>Loading...</p>}
-          endMessage={<p>Больше нет</p>}
-          >
-          {dataByDate.dates.map(date => (
-            <div className="list">
-              <date><div className="today">{date.date}</div></date>
-              <div className="albumslist">
-                {date.albums.map((album, index) => (
-                    <Album album={album} key={index} />
-                  )
-                )}
-              </div>
+        {isEmpty ? <p>Yay, no albums found.</p> : null}
+        {dataByDate.dates.map(date => (
+          <div className='list'>
+            <date><div className='today'>{date.date}</div></date>
+            <div className='albumslist'>
+              {date.albums.map((album, index) => (
+                  <Album album={album} key={index} />
+                )
+              )}
             </div>
-            )
-          )}
-        </InfiniteScroll>
+          </div>
+          )
+        )}
+        <div ref={ref}>
+          {isLoadingMore ? <p>Loading...</p> : isReachingEnd ? <p>No more albums.</p> : ''}
+        </div>
       </section>  
-    </div>
+    </>
   )
 }
+
+// Home.getInitialProps = async () => {
+//   const data = await fetcher('/api/get')
+//   return { props: { initialData: data } }
+// }
 
 export default Home
