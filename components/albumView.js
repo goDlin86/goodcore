7071924e7ea1e32b490ca7e54e7f629e40930101
goDlin <godlin086@gmail.com>
2022-02-24 17:57:19 +0300

    genre electronic

--- components/albumView.js
+++ components/albumView.js
@@ -7,7 +7,7 @@ const AlbumView = ({ album, width, show }) => (
       <a href={'https://vk.com/feed?w=wall' + album.groupid + '_' + album.postid} target='_blank' onClick={e => e.stopPropagation()}>
         <h2>{album.title}</h2>
       </a>
-      {album.genre.substring(6).split('/').map(genre => <div class={styles.genre + (genre.trim() === 'Post-Hardcore' ? (' ' + styles.post_hardcore) : '')}>{genre}</div>)}
+      {album.genre.substring(6).split('/').map(genre => <div class={styles.genre + (genre.trim() === 'Post-Hardcore' ? (' ' + styles.post_hardcore) : genre.trim() === 'Electronic' ? (' ' + styles.electronic) : '')}>{genre}</div>)}
       <div class={styles.country}>{album.country.substring(8)}</div>
     </div>
   </div>
