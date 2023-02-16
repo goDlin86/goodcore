355f300e8d5595e644a01429df46560f22f58d09
Denis <godlin086@gmail.com>
2023-02-16 17:06:30 +0300

    add inngest

---
+++ pages/api/inngest.js
@@ -0,0 +1,21 @@
+import { Inngest } from 'inngest'
+import { serve } from 'inngest/next'
+
+export const inngest = new Inngest({ name: 'goodcore' })
+
+const getAlbums = inngest.createFunction(
+  { name: 'Get albums' }, 
+  { cron: '0 */12 * * *' }, 
+  async ({ event, step }) => {
+    await step.run('db', async () => { 
+      const res = await fetch('https://goodcore.vercel.app/api/db?secret=' + process.env.GOODCORE_TOKEN)
+      return await res.json()
+    })
+    await step.run('revalidate', async () => { 
+      const res = await fetch('https://goodcore.vercel.app/api/revalidate?secret=' + process.env.GOODCORE_TOKEN)
+      return await res.json()
+    })
+  }
+)
+
+export default serve(inngest, [ getAlbums ])
