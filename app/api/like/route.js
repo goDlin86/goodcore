fb1bfa0dedf432043e227d9fc624457b513c6230
Denis <godlin086@gmail.com>
2025-06-04 20:15:22 +0300

    deleted api for fauna

--- app/api/like/route.js
+++ app/api/like/route.js
@@ -1,25 +1,13 @@
-import { NextResponse } from 'next/server'
-import faunadb, { query as q } from 'faunadb'
+
 
 export async function GET(request) {
   const { searchParams } = request.nextUrl
   const id = searchParams.get('id')
   const value = searchParams.get('value')
+  
 
-  const client = new faunadb.Client({ secret: process.env.DBSECRET })
   
-  const ret = await client.query(
-    q.Update(
-      q.Ref(q.Collection('AlbumEntry'), id),
-      {
-        data: {
-          like: value === 'true'
-        }
-      }
-    )
-  )
-  
-  console.log(ret)
+  //console.log(ret)
 
   // .catch((err) => console.error(
   //   'Error: [%s] %s: %s',
@@ -28,5 +16,5 @@ export async function GET(request) {
   //   err.errors()[0].description,
   // ))
 
-  return NextResponse.json(ret)
+  return Response.json({ message: 'Like updated successfully' })
 }
