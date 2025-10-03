19e55befc57dba362480076ae1740926f034839c
Denis <godlin086@gmail.com>
2025-10-03 17:33:27 +0300

    get singles

--- app/api/db/route.js
+++ app/api/db/route.js
@@ -7,20 +7,12 @@ export async function GET(request) {
   const { searchParams } = request.nextUrl
   const secret = searchParams.get('secret')
 
-  if (secret !== process.env.SECRET_TOKEN) return Response.json({ error: 'Invalid token' }, { status: 401 })
+  if (secret !== process.env.SECRET_TOKEN) 
+    return Response.json({ error: 'Invalid token' }, { status: 401 })
 
-  const res = await fetch('https://coreradio.online/albums')
-  const body = await res.text()
-
-  const $ = cheerio.load(body)
-
-  const posts = $('.main-news').toArray().map((item) => ({
-    title: $(item).find('.tcarusel-item-title > a').text(),
-    url: $(item).find('.tcarusel-item-title > a').attr('href'),
-    img: $(item).find('.tcarusel-item-image > a > img').attr('src'),
-    genre: $(item).find('.tcarusel-item-descr2').text().split(/\n/)[1].trim(),
-    country: $(item).find('.tcarusel-item-descr2').text().split(/\n/)[2].trim(),
-  }))
+  const albums = await getPosts('https://coreradio.online/albums')
+  const singles = await getPosts('https://coreradio.online/singles')
+  const posts = [...albums, ...singles]
 
   try {
     const res = await Promise.all(posts.map(async p => {
@@ -38,4 +30,19 @@ export async function GET(request) {
     console.log(e)
     return Response.json({ message: e.message }, { status: 500 })
   }
+}
+
+async function getPosts(url) {
+  const res = await fetch(url)
+  const body = await res.text()
+
+  const $ = cheerio.load(body)
+
+  return $('.main-news').toArray().map((item) => ({
+    title: $(item).find('.tcarusel-item-title > a').text(),
+    url: $(item).find('.tcarusel-item-title > a').attr('href'),
+    img: $(item).find('.tcarusel-item-image > a > img').attr('src'),
+    genre: $(item).find('.tcarusel-item-descr2').text().split(/\n/)[1].trim(),
+    country: $(item).find('.tcarusel-item-descr2').text().split(/\n/)[2].trim(),
+  }))
 }
